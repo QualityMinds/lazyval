eb7cc9c55ac4fc382d9bfd64ba1b00e5bbd68dda
Marc Schlegel <marc.schlegel@gmx.de>
2025-11-04 13:52:27 +0100

    enforce non-nullable type

---
+++ ksp/src/test/resources/test/NullableWrappedType.kt
@@ -0,0 +1,10 @@
+package test
+
+import de.qualityminds.lazyval.LazyValue
+
+/**
+ * A nullable wrapped type is illegal, because you would rather mark the wrapper nullable (not like JPA Embeddables,
+ * which give you a reference to a class with all fields being null)
+ */
+@LazyValue
+class NullableWrappedType (val value: String?) {}
