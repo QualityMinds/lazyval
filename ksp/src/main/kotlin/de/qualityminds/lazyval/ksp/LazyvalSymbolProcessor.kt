81ecf112c3abe03d584e343c00942290f3e132aa
Marc Schlegel <marc.schlegel@gmx.de>
2025-12-22 14:14:18 +0100

    resolve configured values from classpath instead of annotation.
    also fixes errors when dealing with boxed primitives

--- ksp/src/main/kotlin/de/qualityminds/lazyval/ksp/LazyvalSymbolProcessor.kt
+++ ksp/src/main/kotlin/de/qualityminds/lazyval/ksp/LazyvalSymbolProcessor.kt
@@ -1,7 +1,12 @@
 package de.qualityminds.lazyval.ksp
 
-import com.google.devtools.ksp.processing.*
-import com.google.devtools.ksp.symbol.*
+import com.google.devtools.ksp.processing.Dependencies
+import com.google.devtools.ksp.processing.Resolver
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.symbol.KSAnnotated
+import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSFile
 import com.squareup.kotlinpoet.FileSpec
 import de.qualityminds.lazyval.LazyValue
 import de.qualityminds.lazyval.collections.NonEmptySet
