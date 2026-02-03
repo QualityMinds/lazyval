79ce85fced2a9a2590d5fac0f1e71befaa9dcdbc
Marc Schlegel <marc.schlegel@gmx.de>
2026-02-03 10:08:44 +0100

    simplify spi sample to use String template

--- examples/java-spi-dev/custom-lazyval-generator/src/main/java/com/acme/lazyval/generator/UtilsGenerator.java
+++ examples/java-spi-dev/custom-lazyval-generator/src/main/java/com/acme/lazyval/generator/UtilsGenerator.java
@@ -1,15 +1,10 @@
 package com.acme.lazyval.generator;
 
-import com.palantir.javapoet.JavaFile;
-import com.palantir.javapoet.MethodSpec;
-import com.palantir.javapoet.TypeName;
-import com.palantir.javapoet.TypeSpec;
 import de.qualityminds.lazyval.processor.spi.FilePerTypeGenerator;
 import de.qualityminds.lazyval.processor.spi.GeneratorResult;
 import de.qualityminds.lazyval.processor.spi.ValidatedGeneratorElement;
 import org.jspecify.annotations.NullMarked;
 
-import javax.lang.model.element.Modifier;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 import java.util.Collection;
@@ -29,45 +24,37 @@ public class UtilsGenerator implements FilePerTypeGenerator {
     public GeneratorResult generateFilePerType(ValidatedGeneratorElement validatedElement, Settings userSettings) {
 
         TypeElement element = validatedElement.element();
-        TypeMirror type = element.asType();
         TypeMirror wrappedType = validatedElement.wrappedType();
-        TypeName wrappedTypeName;
-        if (wrappedType.getKind().isPrimitive()) {
-            // Box primitive types for JPA generics
-            wrappedTypeName = TypeName.get(wrappedType).box();
-        } else {
-            wrappedTypeName = TypeName.get(wrappedType);
-        }
-
-        if(!wrappedTypeName.equals(TypeName.get(String.class))){
+        // this generator should only handle String types
+        if (!"java.lang.String".equals(wrappedType.toString())) {
             return new GeneratorResult.Nothing();
         }
 
-        final MethodSpec toUppercase = MethodSpec.methodBuilder("toUpperCase")
-                .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
-                .returns(wrappedTypeName)
-                .addParameter(TypeName.get(type), "type")
-                .beginControlFlow("if(type == null)")
-                .addStatement("return null")
-                .endControlFlow()
-                .addStatement(String.format("return type.%s().toUpperCase()", validatedElement.wrappedTypeName()))
-                .build();
-
-
-        TypeSpec someClass = TypeSpec.classBuilder(element.getSimpleName() + "Utils")
-                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
-                .addMethod(toUppercase)
-                .build();
-
-        String userProvidedPackage = userSettings.get(OPTION_GENERATED_PACKAGE)
+        String className = element.getSimpleName() + "Utils";
+        String packageName = userSettings.get(OPTION_GENERATED_PACKAGE)
                 .orElse(String.format("%s.test", extractRootPackage(element)));
-        if(userProvidedPackage.charAt(0) == '.'){
-            userProvidedPackage = userProvidedPackage.substring(1);
+        if(packageName.charAt(0) == '.'){
+            packageName = packageName.substring(1);
         }
-        var javaFile = JavaFile.builder(userProvidedPackage, someClass).build();
+
+        var contents = ("package %s;\n\n".formatted(packageName) +
+                "import %s;\n\n".formatted(element.getQualifiedName()) +
+                "public final class %s {\n".formatted(className) +
+                """
+                            public static %s toUpperCase(%s type) {
+                            if(type == null) {
+                              return null;
+                            }
+                            return type.%s().toUpperCase();
+                          }
+                        """.formatted(
+                                wrappedType.toString(), element.getSimpleName(), validatedElement.wrappedTypeName()) +
+                "}").replaceAll("\\n", System.lineSeparator());
+
+
         return new GeneratorResult.Java(
-                new GeneratorResult.Metadata(userProvidedPackage, someClass.name()),
-                javaFile.toString());
+                new GeneratorResult.Metadata(packageName, className),
+                contents);
     }
 
     @Override
