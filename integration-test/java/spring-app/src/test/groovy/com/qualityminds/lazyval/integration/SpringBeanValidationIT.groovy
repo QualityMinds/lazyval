da8412be22dd9997a4a940e49fd94d2f2c657bb3
Marc Schlegel <marc.schlegel@gmx.de>
2026-07-10 16:11:46 +0200

    add test for NonNull and document unwrapping behavior

--- integration-test/java/spring-app/src/test/groovy/com/qualityminds/lazyval/integration/SpringBeanValidationIT.groovy
+++ integration-test/java/spring-app/src/test/groovy/com/qualityminds/lazyval/integration/SpringBeanValidationIT.groovy
@@ -4,6 +4,7 @@ import com.qualityminds.lazyval.integration.shared.Quantity
 import jakarta.validation.Validator
 import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.valueextraction.Unwrapping
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import spock.lang.Title
@@ -15,7 +16,7 @@ class SpringBeanValidationIT extends AbstractIT {
     @Autowired
     Validator validator
 
-    def "default constraint is running on domain-primitive and returning a violation"() {
+    def "domain-primitive is unwrapped for default constraints"() {
         given:
         def some = new SomeClass(new Quantity(11))
 
@@ -31,12 +32,48 @@ class SpringBeanValidationIT extends AbstractIT {
         }
     }
 
+    def "NonNull works for the call-site being null, not the internal value, when unwrap is skipped"() {
+        given:
+        def some = new SomeClass(null)
+
+        when:
+        def violations = validator.validate(some)
+
+        then:
+        violations.size() == 1
+        with(violations.first()) {
+            propertyPath.toString() == "quantity"
+            constraintDescriptor.annotation.annotationType() == NotNull
+            invalidValue == null
+        }
+    }
+
+    def "NonNull is skipped on the reference as unwrap is never called - no violation"() {
+        given:
+        def some = new NonNullSkippedClass(null)
+
+        when:
+        def violations = validator.validate(some)
+
+        then:
+        violations.size() == 0
+    }
+
     static class SomeClass {
-        @NotNull
+        @NotNull(payload = Unwrapping.Skip.class)
         @Max(10)
         Quantity quantity
 
         SomeClass(Quantity quantity) {
+            this.quantity = quantity
+        }
+    }
+
+    static class NonNullSkippedClass {
+        @NotNull
+        Quantity quantity
+
+        NonNullSkippedClass(Quantity quantity) {
             this.quantity = quantity
         }
     }
