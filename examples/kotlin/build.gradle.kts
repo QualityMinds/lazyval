abddbb6fbca8f1abb158914d89730ee0e6b34cbd
Marc Schlegel <marc.schlegel@gmx.de>
2026-07-13 15:13:30 +0200

    downgrade kotlin & ksp version in examples
    Kotlin 2.1 still mostly used, hence test examples against this version.

--- examples/kotlin/build.gradle.kts
+++ examples/kotlin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.20"
-    id("com.google.devtools.ksp") version "2.3.6"
+    kotlin("jvm") version "2.1.21"
+    id("com.google.devtools.ksp") version "2.1.21-2.0.2"
 }
 
 // tag::docu[]
